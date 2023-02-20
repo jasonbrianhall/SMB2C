f1dd0cda3c3718a2f2c10bf832b9df682d8a647b
jasonbrianhall@gmail.com <jbhall@fbi.gov>
2023-02-19 20:35:31 -0500

    Latest

--- ASM2C.py
+++ ASM2C.py
@@ -1,82 +1,158 @@
 import json
+import instructionset
 
-debug=False
+instructiondata={
+	"adc": instructionset.adc,
+	"and": instructionset.and1,
+	"asl": instructionset.asl,
+	"bcc": instructionset.bcc,
+	"bcs": instructionset.bcs,
+	"beq": instructionset.beq,
+	"bit": instructionset.bit,
+	"bmi": instructionset.bmi,
+	"bne": instructionset.bne,
+	"bpl": instructionset.bpl,
+	"clc": instructionset.clc,
+	"cld": instructionset.cld,
+	"cmp": instructionset.cmp,
+	"cpx": instructionset.cpx,
+	"cpy": instructionset.cpy,
+	"dec": instructionset.dec,
+	"dex": instructionset.dex,
+	"dey": instructionset.dey,
+	".dw": instructionset.dw1,
+	"eor": instructionset.eor,
+	"inc": instructionset.inc,
+	"index": instructionset.index,
+	"inx": instructionset.inx,
+	"iny": instructionset.iny,
+	"jmp": instructionset.jmp,
+	"jsr": instructionset.jsr,
+	"lda": instructionset.lda,
+	"ldx": instructionset.ldx,
+	"ldy": instructionset.ldy,
+	"lsr": instructionset.lsr,
+	"mem": instructionset.mem,
+	"ora": instructionset.ora,
+	"org": instructionset.org,
+	"pha": instructionset.pha,
+	"pla": instructionset.pla,
+	"rol": instructionset.rol,
+	"ror": instructionset.ror,
+	"rti": instructionset.rti,
+	"rts": instructionset.rts,
+	"sbc": instructionset.sbc,
+	"sec": instructionset.sec,
+	"sei": instructionset.sei,
+	"sta": instructionset.sta,
+	"stx": instructionset.stx,
+	"sty": instructionset.sty,
+	"tax": instructionset.tax,
+	"tay": instructionset.tay,
+	"txa": instructionset.txa,
+	"txs": instructionset.txs,
+	"tya": instructionset.tya
+}
 
-f=open("smb.asm", "r")
-data=f.read().split("\n")
 
-# The folloowing code converts the ASM to a Dictionary
-instructions={}
-counter=1
-skipcounter=False
-linenumber=0
-for line in data:
-	linenumber+=1
-	temp=line.split(";", 1)
-	if skipcounter==False:
-		instructions[counter]={}	
+def main():
+	debug=False
+
+	f=open("smb.asm", "r")
+	data=f.read().split("\n")
+
+	# The folloowing code converts the ASM to a Dictionary
+	instructions={}
+	counter=1
 	skipcounter=False
-	if len(temp)>1:
-		instructions[counter]["comment"]=temp[1].strip()
-	newdata=temp[0]
-	temp=newdata.split("=")
-	if len(temp)>1:
-		instructions[counter]["variable"]=temp[0].strip()
-		instructions[counter]["variablevalue"]=temp[1].strip()
-	else:
+	linenumber=0
+	for line in data:
+		linenumber+=1
+		temp=line.split(";", 1)
+		if skipcounter==False:
+			instructions[counter]={}	
+		skipcounter=False
+		if len(temp)>1:
+			instructions[counter]["comment"]=temp[1].strip()
 		newdata=temp[0]
-		temp=newdata.split(":")
+		temp=newdata.split("=")
 		if len(temp)>1:
-			if len(temp)>1:
-				instructions[counter]["jumplocation"]=temp[0].strip()
-				newdata=temp[1].strip()
-				skipcounter=True
-			else:
-				newdata=temp[0].strip()
-		temp=newdata.strip().split(" ")
-
-		if len(temp)>1:
-			if temp[0]==".db":
-				for x in range(1, len(temp)):
-					if instructions.get(counter).get(".db")==None:
-						instructions[counter][".db"]=[]
-						if not instructions.get(counter).get("jumplocation")==None:
-							instructions[counter]["dbloc"]=instructions[counter]["jumplocation"]
-							del instructions[counter]["jumplocation"]
-						instructions[counter][".db"].append(temp[x].strip(","))
-						
-					else:
-						instructions[counter][".db"].append(temp[x].strip(","))
-						skipcounter=True
-			else:
-				instructions[counter]["instruction"]=temp[0].strip()
-				instructions[counter]["instructionvalue"]=temp[1].strip()
-				skipcounter=False
+			instructions[counter]["variable"]=temp[0].strip()
+			instructions[counter]["variablevalue"]=temp[1].strip()
 		else:
-			if not temp[0]=="":
-				instructions[counter]["instruction"]=temp[0].strip()
-				skipcounter=False
-		#print(temp)
+			newdata=temp[0]
+			temp=newdata.split(":")
+			if len(temp)>1:
+				if len(temp)>1:
+					instructions[counter]["jumplocation"]=temp[0].strip()
+					newdata=temp[1].strip()
+					skipcounter=True
+				else:
+					newdata=temp[0].strip()
+			temp=newdata.strip().split(" ")
+
+			if len(temp)>1:
+				if temp[0]==".db":
+					for x in range(1, len(temp)):
+						if instructions.get(counter).get(".db")==None:
+							instructions[counter][".db"]=[]
+							if not instructions.get(counter).get("jumplocation")==None:
+								instructions[counter]["dbloc"]=instructions[counter]["jumplocation"]
+								del instructions[counter]["jumplocation"]
+							instructions[counter][".db"].append(temp[x].strip(","))
+							
+						else:
+							instructions[counter][".db"].append(temp[x].strip(","))
+							skipcounter=True
+				else:
+					if temp[0].strip()==".index":
+						temp[0]="index"
+					elif temp[0].strip()==".mem":
+						temp[0]="mem"
+					elif temp[0].strip()==".org":
+						temp[0]="org"
+					instructions[counter]["instruction"]=temp[0].strip()
+					instructions[counter]["instructionvalue"]=temp[1].strip()
+					skipcounter=False
+			else:
+				if not temp[0]=="":
+					instructions[counter]["instruction"]=temp[0].strip()
+					skipcounter=False
+			#print(temp)
+				
 			
+		if skipcounter==False:					
+			counter=linenumber
+				
+	if debug==True:
+		print(json.dumps(instructions, indent=4))
 		
-	if skipcounter==False:					
-		counter=linenumber
-			
-if debug==True:
-	print(json.dumps(instructions, indent=4))
-	
-	
-buffer="""#include <stdio.h>
-#include <stdlib.h>
-int main(void) {
 
-	char memory[8192];
-"""
-for instruction in instructions:
-	temp=instructions.get(instruction)
-	if not temp.get("comment")==None:
-		buffer=buffer + "\t// " + temp.get("comment") + "\n"
-	
-buffer=buffer + "\n\treturn 0;\n}"
+	buffer="""#include <stdio.h>
+	#include <stdlib.h>
+	int main(void) {
 
-print(buffer)
+		char memory[8192];
+	"""
+
+		
+
+	for instruction in instructions:
+		temp=instructions.get(instruction)
+		if not temp.get("comment")==None:
+			buffer=buffer + "\t// " + temp.get("comment") + "\n"
+		if not temp.get("jumplocation")==None:
+			buffer=buffer + "\t" + temp.get("jumplocation") + ":" + "\n"
+		if not temp.get("instruction")==None:
+			if not temp.get("instructionvalue")==None:
+				#print(temp.get("instruction"))
+				buffer=instructiondata.get(temp.get("instruction"))(buffer, temp.get("instructionvalue"))
+			else:
+				#print(temp.get("instruction"))
+				buffer=instructiondata.get(temp.get("instruction"))(buffer)
+	
+	buffer=buffer + "\n\treturn 0;\n}"
+	#print(buffer)
+	
+if __name__ == "__main__":
+	main()
---
+++ instructionset.py
@@ -0,0 +1,113 @@
+def adc(buffer, value=None):
+	return buffer
+def and1(buffer, value=None):
+	return buffer
+def asl(buffer, value=None):
+	return buffer
+def bcc(buffer, value=None):
+	return buffer
+def bcs(buffer, value=None):
+	return buffer
+def beq(buffer, value=None):
+	return buffer
+def bit(buffer, value=None):
+	return buffer
+def bmi(buffer, value=None):
+	return buffer
+def bne(buffer, value=None):
+	return buffer
+def bpl(buffer, value=None):
+	return buffer
+def clc(buffer):
+	return buffer
+
+def cld(buffer):
+	return buffer
+
+def cmp(buffer, value=None):
+	return buffer
+def cpx(buffer, value=None):
+	return buffer
+def cpy(buffer, value=None):
+	return buffer
+def dec(buffer, value=None):
+	return buffer
+
+def dex(buffer):
+	return buffer
+
+def dey(buffer):
+	return buffer
+
+def dw1(buffer, value=None):
+	return buffer
+def eor(buffer, value=None):
+	return buffer
+def inc(buffer, value=None):
+	return buffer
+def index(buffer, value=None):
+	return buffer
+
+def inx(buffer):
+	return buffer
+
+def iny(buffer):
+	return buffer
+def jmp(buffer, value=None):
+	return buffer
+def jsr(buffer, value=None):
+	return buffer
+def lda(buffer, value=None):
+	return buffer
+def ldx(buffer, value=None):
+	return buffer
+def ldy(buffer, value=None):
+	return buffer
+def lsr(buffer, value=None):
+	return buffer
+def mem(buffer, value=None):
+	return buffer
+def ora(buffer, value=None):
+	return buffer
+def org(buffer, value=None):
+	return buffer
+
+def pha(buffer, value=None):
+	return buffer
+
+def pla(buffer):
+	return buffer
+
+def rol(buffer, value=None):
+	return buffer
+def ror(buffer, value=None):
+	return buffer
+def rti(buffer):
+	return buffer
+def rts(buffer):
+	return buffer
+def sbc(buffer, value=None):
+	return buffer
+def sec(buffer):
+	return buffer
+def sei(buffer):
+	return buffer
+def sta(buffer, value=None):
+	return buffer
+def stx(buffer, value=None):
+	return buffer
+def sty(buffer, value=None):
+	return buffer
+def tax(buffer, value=None):
+	return buffer
+def tay(buffer, value=None):
+	return buffer
+def txa(buffer, value=None):
+	return buffer
+
+def txs(buffer):
+	return buffer
+
+def tya(buffer, value=None):
+	return buffer
+	
